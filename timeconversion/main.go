a39a9ffbbcade6ce420edfdbe707830d852e995b
Waldemar Bira <waldek.bira@gmail.com>
2020-03-29 13:06:03 +0200

    Solved timeconversion

---
+++ timeconversion/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the timeConversion function below.
+ */
+func timeConversion(s string) string {
+	suffix := s[8:]
+	splited := strings.Split(s[:8], ":")
+
+	hours, _ := strconv.Atoi(splited[0])
+	if suffix == "PM" && hours != 12 {
+		hours += 12
+	}
+
+	if suffix == "AM" && hours == 12 {
+		hours = 0
+	}
+
+	return fmt.Sprintf("%02d:%s:%s", hours, splited[1], splited[2])
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	outputFile, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer outputFile.Close()
+
+	writer := bufio.NewWriterSize(outputFile, 1024*1024)
+
+	s := readLine(reader)
+
+	result := timeConversion(s)
+	fmt.Println(result)
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
